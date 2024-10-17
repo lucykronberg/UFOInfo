e4b3ccdf4753cb705bcc4c20ebb95d7af7f46057
lucykronberg <lucykronberg@gmail.com>
2024-10-17 13:40:15 -0700

    update

--- webapp.py
+++ webapp.py
@@ -1,8 +1,18 @@
-numbers = [ orb, triangle,other, fireball, disc, egg, cigar, changing, rectangle, diamond]
+from flask import Flask, request, url_for, render_template, flash
+from markupsafe import Markup
+from datetime import datetime
+
+import os
+import json
+
+app = Flask(__name__)
+
+with open('ufo_sightings.json') as ufo_data:
+    sightings = json.load(ufo_data)
 counts = {}
 for num in numbers:
     if num in counts:
         counts[num] = counts[num] + 1
     else:
-    counts[num] = 1
+        counts[num] = 1
     
